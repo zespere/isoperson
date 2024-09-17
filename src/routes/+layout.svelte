5cfb6fef253f2c1c075e4c1b0696499af625fc2b
Wojciech Wozniak <inzynierz8@gmail.com>
2024-09-17 23:12:42 +0200

    probably works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,19 @@
 <script>
- import "../app.css";
+	import '../app.css';
 </script>
 
 <head>
-  <title>Test osobowości ISO/OSI</title>
+	<title>Test osobowości ISO/OSI</title>
 </head>
 
 <div class="flex flex-col min-h-screen">
-<main class="max-w-4xl px-4 mx-auto flex-grow">
-<slot />
-</main>
+	<main class="max-w-4xl px-4 mx-auto flex-grow">
+		<slot />
+	</main>
 
-<footer class="py-2 bg-zinc-200 border border-t border-zinc-400 mt-auto">
-  <p class="text-center font-size-xl">W budowę strony pracę i <span class="text-red-500 text-2xl">&#10084</span> włożył Wojciech Woźniak</p>
-</footer>
+	<footer class="py-2 bg-zinc-200 border border-t border-zinc-400 mt-auto">
+		<p class="text-center font-size-xl">
+			W budowę strony pracę i <span class="text-red-500 text-2xl">&#10084</span> włożył Wojciech Woźniak
+		</p>
+	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,173 +1,235 @@
 <script lang="ts">
-  let questions = [
-    {
-      question: "W komunikacji jesteś zwięzły/a?",
-      options: ["Odp 1", "Od zawsze fascynowało mnie bogactwo i różnorodność języka polskiego, a szczególnie jego zdolność do tworzenia długich, złożonych wyrazów, które mogą wydawać się nieco przesadne, a wręcz niepotrzebne w codziennej komunikacji. Używanie takich słów sprawia mi ogromną przyjemność, ponieważ pozwala mi na wyrażenie myśli w sposób, który jest zarówno kreatywny, jak i nieco zabawny. W moim odczuciu, długie słowa mają w sobie pewną magię. Kiedy wypowiadam je na głos, czuję, jakby język tańczył w moich ustach, a każda sylaba dodaje nowy wymiar do mojej wypowiedzi. Na przykład, zamiast powiedzieć 'zrozumieć', mogę użyć 'przyswoić sobie złożoność zagadnienia', co nie tylko wydłuża zdanie, ale także nadaje mu pewną powagę i głębię. Często zastanawiam się, dlaczego tak bardzo pociągają mnie te nieco pretensjonalne wyrazy. Może to kwestia chęci wyróżnienia się w tłumie, pragnienia, aby moje wypowiedzi były zapamiętane i docenione. W świecie, w którym króluje szybkość i zwięzłość, ja wybieram drogę, która prowadzi przez labirynty językowych konstrukcji, gdzie każdy zakręt odkrywa nowe, nieznane dotąd słowa."],
-      scores: ["physical", "presentation"]
-    },
-    {
-      question: "Czy podczas rozmowy z innymi dodajesz sumy kontrolne?",
-      options: ["Tak", "Nie", "Integrity Check Failure"],
-      scores: ["transport", "application", "transport"]
-    },
-    {
-      question: "Jak się komunikujesz?",
-      options: ["Bezpośrednio, mówię jak myślę", "Przed wypowiedzeniem czegoś staram się sformatować wiadomość w sposób zrozumiały dla odbiorcy.", "Skupiam się na podzieleniu przekazywanych informacji w zdania które mają sens.", "Nie zastanawiam się nad tym, ważne że rozmawiam z właściwą osobą."],
-      scores: ["physical", "presentation", "session", "transport"]
-    },
-    {
-      question: "Co robisz gdy napotykasz problemy?",
-      options: ["Ignoruję je", "Gdy go zauważę zależnie od okoliczności dzielę się wiadomością ICMP z bliskimi", "Jestem bezsilny"],
-      scores: ["application", "network", "physical"]
-    },
-    {
-      question: "Strona się nie ładuje, co robisz?",
-      options: ["Resetuję przeglądarkę", "Wyłączam komputer", "Idę dotknąć trawy (???)", "Usuwam ciasteczka"],
-      scores: ["application", "physical", "data_link", "session"]
-    },
-    {
-      question: "Jaki jest twój stosunek do poczucia bezpieczeństwa?",
-      options: ["Zaklejam kamerę taśmą klejącą", "Korzystam z VPN'a", "TLS+SSL"],
-      scores: ["physical", "network", "transport"]
-    },
-    {
-      question: "Jak spędzasz czas wolny?",
-      options: ["Duże grono znajomych z trwałymi znajomościami.", "Niezależność i ciągłą możliwość zmiany ścieżki.", "Ekspresję samego siebie i kreatywność.", "Spójność i szczerość"],
-      scores: ["session", "network", "presentation", "data_link"]
-    },
-    {
-      question: "Jaki jest twój sposób na okazywanie wdzięczności?",
-      options: ["KABEL SIECIOWY LAN PATCHCORD ZŁOTY MIEDŹ + SFTP CAT7 ROUTER kat 7 RJ45 10M", " Shrek.2.2004.PL.1080p.HD.WEBRip.1.09GiB.AAC.x264-PortalGoods ", "Zniżka 10% na wymianę RAM'u"],
-      scores: ["physical", "application", "data_link"]
-    },
-    {
-      question: "Kiedy zdobywasz energię?",
-      options: ["Gdy ktoś podłączy wtyczkę", "Komunikacja z najbliższymi"],
-      scores: ["physical", "session"]
-    },
-    {
-      question: "Czego się najbardziej boisz?",
-      options: ["Ryszard Masztalerz", "Inne"],
-      scores: ["network", "application"]
-    },
-    {
-      question: "Czym wg. Ciebie jest sukces?",
-      options: ["200", "01110000011101000110111101110011011110100110010101101011001011100111000001101100", "nie mam pomysłu na odpowiedź :/", "asdhawuioawd"],
-      scores: ["application", "data_link", "presentation", "physical"]
-    }
-  ];
+	let questions = [
+		{
+			question: 'W komunikacji jesteś zwięzły/a?',
+			options: [
+				'Odp 1',
+				"Od zawsze fascynowało mnie bogactwo i różnorodność języka polskiego, a szczególnie jego zdolność do tworzenia długich, złożonych wyrazów, które mogą wydawać się nieco przesadne, a wręcz niepotrzebne w codziennej komunikacji. Używanie takich słów sprawia mi ogromną przyjemność, ponieważ pozwala mi na wyrażenie myśli w sposób, który jest zarówno kreatywny, jak i nieco zabawny. W moim odczuciu, długie słowa mają w sobie pewną magię. Kiedy wypowiadam je na głos, czuję, jakby język tańczył w moich ustach, a każda sylaba dodaje nowy wymiar do mojej wypowiedzi. Na przykład, zamiast powiedzieć 'zrozumieć', mogę użyć 'przyswoić sobie złożoność zagadnienia', co nie tylko wydłuża zdanie, ale także nadaje mu pewną powagę i głębię. Często zastanawiam się, dlaczego tak bardzo pociągają mnie te nieco pretensjonalne wyrazy. Może to kwestia chęci wyróżnienia się w tłumie, pragnienia, aby moje wypowiedzi były zapamiętane i docenione. W świecie, w którym króluje szybkość i zwięzłość, ja wybieram drogę, która prowadzi przez labirynty językowych konstrukcji, gdzie każdy zakręt odkrywa nowe, nieznane dotąd słowa."
+			],
+			scores: ['physical', 'presentation']
+		},
+		{
+			question: 'Czy podczas rozmowy z innymi dodajesz sumy kontrolne?',
+			options: ['Tak', 'Nie', 'Integrity Check Failure'],
+			scores: ['transport', 'application', 'transport']
+		},
+		{
+			question: 'Jak się komunikujesz?',
+			options: [
+				'Bezpośrednio, mówię jak myślę',
+				'Przed wypowiedzeniem czegoś staram się sformatować wiadomość w sposób zrozumiały dla odbiorcy.',
+				'Skupiam się na podzieleniu przekazywanych informacji w zdania które mają sens.',
+				'Nie zastanawiam się nad tym, ważne że rozmawiam z właściwą osobą.'
+			],
+			scores: ['physical', 'presentation', 'session', 'transport']
+		},
+		{
+			question: 'Co robisz gdy napotykasz problemy?',
+			options: [
+				'Ignoruję je',
+				'Gdy go zauważę zależnie od okoliczności dzielę się wiadomością ICMP z bliskimi',
+				'Jestem bezsilny'
+			],
+			scores: ['application', 'network', 'physical']
+		},
+		{
+			question: 'Strona się nie ładuje, co robisz?',
+			options: [
+				'Resetuję przeglądarkę',
+				'Wyłączam komputer',
+				'Idę dotknąć trawy (???)',
+				'Usuwam ciasteczka'
+			],
+			scores: ['application', 'physical', 'data_link', 'session']
+		},
+		{
+			question: 'Jaki jest twój stosunek do poczucia bezpieczeństwa?',
+			options: ['Zaklejam kamerę taśmą klejącą', "Korzystam z VPN'a", 'TLS+SSL'],
+			scores: ['physical', 'network', 'transport']
+		},
+		{
+			question: 'Jak spędzasz czas wolny?',
+			options: [
+				'Duże grono znajomych z trwałymi znajomościami.',
+				'Niezależność i ciągłą możliwość zmiany ścieżki.',
+				'Ekspresję samego siebie i kreatywność.',
+				'Spójność i szczerość'
+			],
+			scores: ['session', 'network', 'presentation', 'data_link']
+		},
+		{
+			question: 'Jaki jest twój sposób na okazywanie wdzięczności?',
+			options: [
+				'KABEL SIECIOWY LAN PATCHCORD ZŁOTY MIEDŹ + SFTP CAT7 ROUTER kat 7 RJ45 10M',
+				' Shrek.2.2004.PL.1080p.HD.WEBRip.1.09GiB.AAC.x264-PortalGoods ',
+				"Zniżka 10% na wymianę RAM'u"
+			],
+			scores: ['physical', 'application', 'data_link']
+		},
+		{
+			question: 'Kiedy zdobywasz energię?',
+			options: ['Gdy ktoś podłączy wtyczkę', 'Komunikacja z najbliższymi'],
+			scores: ['physical', 'session']
+		},
+		{
+			question: 'Czego się najbardziej boisz?',
+			options: ['Ryszard Masztalerz', 'Inne'],
+			scores: ['network', 'application']
+		},
+		{
+			question: 'Czym wg. Ciebie jest sukces?',
+			options: [
+				'200',
+				'01110000011101000110111101110011011110100110010101101011001011100111000001101100',
+				'nie mam pomysłu na odpowiedź :/',
+				'asdhawuioawd'
+			],
+			scores: ['application', 'data_link', 'presentation', 'physical']
+		}
+	];
 
-  let startTest = false;
-  let answers = new Array(questions.length).fill(null);
+	let startTest = false;
+	let answers = new Array(questions.length).fill(null);
 
-  let result = "";
-  let desc = "Nieznany błąd, przepraszam :^(";
+	let result = '';
+	let desc = 'Nieznany błąd, przepraszam :^(';
 
-  let physical = 0;
-  let data_link = 0;
-  let network = 0;
-  let transport = 0;
-  let session = 0;
-  let presentation = 0;
-  let application = 0;
+	let physical = 0;
+	let data_link = 0;
+	let network = 0;
+	let transport = 0;
+	let session = 0;
+	let presentation = 0;
+	let application = 0;
 
-  function start() {
-    startTest = true;
-  }
+	function start() {
+		startTest = true;
+	}
 
-  function submit() {
-    // Reset all scores
-    physical = data_link = network = transport = session = presentation = application = 0;
+	function submit() {
+		// Reset all scores
+		physical = data_link = network = transport = session = presentation = application = 0;
 
-    // Calculate scores based on answers
-    answers.forEach((answer, index) => {
-      if (answer !== null) {
-        const score = questions[index].scores[answer];
-        switch (score) {
-          case "physical": physical++; break;
-          case "data_link": data_link++; break;
-          case "network": network++; break;
-          case "transport": transport++; break;
-          case "session": session++; break;
-          case "presentation": presentation++; break;
-          case "application": application++; break;
-        }
-      }
-    });
+		// Calculate scores based on answers
+		answers.forEach((answer, index) => {
+			if (answer !== null) {
+				const score = questions[index].scores[answer];
+				switch (score) {
+					case 'physical':
+						physical++;
+						break;
+					case 'data_link':
+						data_link++;
+						break;
+					case 'network':
+						network++;
+						break;
+					case 'transport':
+						transport++;
+						break;
+					case 'session':
+						session++;
+						break;
+					case 'presentation':
+						presentation++;
+						break;
+					case 'application':
+						application++;
+						break;
+				}
+			}
+		});
 
-    // Determine the highest score
-    const scores = {
-      "Warstwa fizyczna": physical,
-      "Warstwa łącza danych": data_link,
-      "Warstwa sieciowa": network,
-      "Warstwa transportowa": transport,
-      "Warstwa sesji": session,
-      "Warstwa prezentacji": presentation,
-      "Warstwa aplikacji": application
-    };
+		// Determine the highest score
+		const scores = {
+			'Warstwa fizyczna': physical,
+			'Warstwa łącza danych': data_link,
+			'Warstwa sieciowa': network,
+			'Warstwa transportowa': transport,
+			'Warstwa sesji': session,
+			'Warstwa prezentacji': presentation,
+			'Warstwa aplikacji': application
+		};
 
-    result = Object.keys(scores).reduce((a, b) => scores[a] > scores[b] ? a : b);
+		result = Object.keys(scores).reduce((a, b) => (scores[a] > scores[b] ? a : b));
 
-    // Set description based on result
-    switch (result) {
-      case "Warstwa fizyczna":
-        desc = "Jesteś osobą bardzo praktyczną i zorientowaną na konkretne działania.";
-        break;
-      case "Warstwa łącza danych":
-        desc = "Masz talent do zapewniania niezawodnej komunikacji i dbasz o szczegóły.";
-        break;
-      case "Warstwa sieciowa":
-        desc = "Jesteś świetnym strategiem, umiejącym łączyć różne elementy w spójną całość.";
-        break;
-      case "Warstwa transportowa":
-        desc = "Twoja mocna strona to zapewnianie płynnej komunikacji i zarządzanie zasobami.";
-        break;
-      case "Warstwa sesji":
-        desc = "Masz talent do nawiązywania i utrzymywania relacji, jesteś świetnym mediatorem.";
-        break;
-      case "Warstwa prezentacji":
-        desc = "Jesteś kreatywną osobą z talentem do prezentowania informacji w zrozumiały sposób.";
-        break;
-      case "Warstwa aplikacji":
-        desc = "Masz zdolność do tworzenia praktycznych rozwiązań i jesteś zorientowany/a na użytkownika.";
-        break;
-    }
-  }
+		// Set description based on result
+		switch (result) {
+			case 'Warstwa fizyczna':
+				desc = 'Jesteś osobą bardzo praktyczną i zorientowaną na konkretne działania.';
+				break;
+			case 'Warstwa łącza danych':
+				desc = 'Masz talent do zapewniania niezawodnej komunikacji i dbasz o szczegóły.';
+				break;
+			case 'Warstwa sieciowa':
+				desc = 'Jesteś świetnym strategiem, umiejącym łączyć różne elementy w spójną całość.';
+				break;
+			case 'Warstwa transportowa':
+				desc = 'Twoja mocna strona to zapewnianie płynnej komunikacji i zarządzanie zasobami.';
+				break;
+			case 'Warstwa sesji':
+				desc = 'Masz talent do nawiązywania i utrzymywania relacji, jesteś świetnym mediatorem.';
+				break;
+			case 'Warstwa prezentacji':
+				desc = 'Jesteś kreatywną osobą z talentem do prezentowania informacji w zrozumiały sposób.';
+				break;
+			case 'Warstwa aplikacji':
+				desc =
+					'Masz zdolność do tworzenia praktycznych rozwiązań i jesteś zorientowany/a na użytkownika.';
+				break;
+		}
+	}
 </script>
 
 <!-- O ty hakierze jeden widzę że HTML'a przeglądasz :o -->
 
 <div>
-  <h1 class="bg-blue-400 text-5xl text-white font-bold text-center py-12 rounded-b-2xl">Test osobowości ISO/OSI</h1>
-  {#if startTest}
-  {#each questions as { question, options }, index}
-    <div class="py-12">
-      <h2 class="p-1 text-lg mb-1">{question}</h2>
-      <div class="flex flex-col space-y-2">
-        {#each options as option, optionIndex}
-          <label class="p-2 bg-zinc-200 rounded-lg">
-            <input type="radio" bind:group={answers[index]} value={optionIndex} />
-            {option}
-          </label>
-        {/each}
-      </div>
-    </div>
-  {/each}
-  <button on:click={submit} class="bg-green-500 w-full mb-12 h-12 rounded-lg text-white text-xl font-medium md:w-72 md:mx-auto block">Otrzymaj wynik</button>
-  {#if result}
-    <section class="p-8 bg-zinc-200 rounded-xl mb-12">
-    <h2 class="text-2xl mb-8">Jesteś warstwą {result}!</h2>
-    <p class="">{desc}</p>
-    </section>
-  {/if}
-  {:else}
-  <div class="py-16 text-xl">
-  <p> Miło mi Ciebie tu widzieć! </p> <br />
-  <p> Chciałbym Cię zaprosić do zmuszającego do myślenia wyjątkowego doświadczenia jakim jest test osobowości oparty na modelu ISO/OSI dzięki któremu odkryjesz nigdy wczęściej nieznane ci zakamarki swojego <i><b>ja</b></i>. </p> <br/>
-  <p> Dziękuję za uwagę i życzę miłej podróży. </p>
-  </div>
-  <button on:click={start} class="bg-blue-400 w-full mb-12 h-12 rounded-lg text-white text-xl font-medium md:w-72 md:mx-auto block">Rozpocznij</button>
-  {/if}
+	<h1 class="bg-blue-400 text-5xl text-white font-bold text-center py-12 rounded-b-2xl">
+		Test osobowości ISO/OSI
+	</h1>
+	{#if startTest}
+		{#each questions as { question, options }, index}
+			<div class="py-12">
+				<h2 class="p-1 text-lg mb-1">{question}</h2>
+				<div class="flex flex-col space-y-2">
+					{#each options as option, optionIndex}
+						<label class="p-2 bg-zinc-200 rounded-lg">
+							<input type="radio" bind:group={answers[index]} value={optionIndex} />
+							{option}
+						</label>
+					{/each}
+				</div>
+			</div>
+		{/each}
+		<button
+			on:click={submit}
+			class="bg-green-500 w-full mb-12 h-12 rounded-lg text-white text-xl font-medium md:w-72 md:mx-auto block"
+			>Otrzymaj wynik</button
+		>
+		{#if result}
+			<section class="p-8 bg-zinc-200 rounded-xl mb-12">
+				<h2 class="text-2xl mb-8">Jesteś warstwą {result}!</h2>
+				<p class="">{desc}</p>
+			</section>
+		{/if}
+	{:else}
+		<div class="py-16 text-xl">
+			<p>Miło mi Ciebie tu widzieć!</p>
+			<br />
+			<p>
+				Chciałbym Cię zaprosić do zmuszającego do myślenia wyjątkowego doświadczenia jakim jest test
+				osobowości oparty na modelu ISO/OSI dzięki któremu odkryjesz nigdy wczęściej nieznane ci
+				zakamarki swojego <i><b>ja</b></i>.
+			</p>
+			<br />
+			<p>Dziękuję za uwagę i życzę miłej podróży.</p>
+		</div>
+		<button
+			on:click={start}
+			class="bg-blue-400 w-full mb-12 h-12 rounded-lg text-white text-xl font-medium md:w-72 md:mx-auto block"
+			>Rozpocznij</button
+		>
+	{/if}
 </div>
 
 <!-- R.I.P. Stefan -->
